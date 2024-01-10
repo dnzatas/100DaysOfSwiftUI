516bb4bf95da2c2457c5ccc70fc9b10070611b8f
Deniz Ataş <denizatas.18@gmail.com>
2024-01-10 17:40:48 +0300

    Add files via upload

---
+++ P02-GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by deniz on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
